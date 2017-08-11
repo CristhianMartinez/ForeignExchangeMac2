4a1130c188e6ac406944c9ea4d3d24f439177925
Cristhian Martinez <cristhianmartinez@SBSCO2s-MacBook-Pro.local>
2017-08-11 11:28:59 -0500

    Add MVVM arch

---
+++ ForeignExchangeMac2/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,20 @@
+﻿namespace ForeignExchangeMac2.Infrastructure
+{
+    using ViewModels;
+
+    public class InstanceLocator
+    {
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+
+        
+    }
+}
---
+++ ForeignExchangeMac2/ViewModels/MainViewModel.cs
@@ -0,0 +1,112 @@
+﻿namespace ForeignExchangeMac2.ViewModels
+{
+	using System;
+	using System.ComponentModel;
+	using System.Windows.Input;
+	using GalaSoft.MvvmLight.Command;
+	using Xamarin.Forms;
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Attributes
+        string _dollars;
+		string _euros;
+		string _pounds;
+		#endregion
+
+		#region Properties
+		public string Pesos
+        {
+            get;
+            set;
+        }
+        public string Dollars
+        {
+            get 
+            { 
+                return _dollars;  
+            }
+            set
+            {
+                if(value != _dollars){
+                    _dollars = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Dollars)));
+                }  
+            }
+        }
+        public string Euros
+        {
+			get
+			{
+                return _euros;
+			}
+			set
+			{
+				if (value != _euros)
+				{
+					_euros = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Euros)));
+				}
+			}
+        }
+        public string Pounds
+        {
+			get
+			{
+                return _pounds;
+			}
+			set
+			{
+				if (value != _pounds)
+				{
+					_pounds = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Pounds)));
+				}
+			}
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+        }
+        #endregion
+
+        #region Commands
+        public ICommand ConvertCommand
+        {
+            get { return new RelayCommand(Convert);  }
+        }
+
+        async void Convert()
+        {
+			if (string.IsNullOrEmpty(Pesos))
+			{
+                await Application.Current.MainPage.DisplayAlert("Error", "You must enter a value in Pesos", "Acept");
+				return;
+			}
+
+			decimal pesos = 0;
+			if (!decimal.TryParse(Pesos, out pesos))
+			{
+				await Application.Current.MainPage.DisplayAlert("Error", "You must enter a value numeric in Pesos", "Acept");
+				Pesos = null;
+				return;
+			}
+
+			var dollars = pesos / 3003.003M;
+			var euros = pesos / 3531.05105M;
+			var pounds = pesos / 3907.23724M;
+
+			Dollars = string.Format("${0:N2}", dollars);
+			Euros = string.Format("€{0:N2}", euros);
+			Pounds = string.Format("£{0:N2}", pounds);
+
+		}
+        #endregion
+    }
+}
